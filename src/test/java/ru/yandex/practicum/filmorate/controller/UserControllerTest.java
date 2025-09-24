631a35b353c189d7daa13e284a35b35ff37dee5e
Kolidgio <>
2025-09-24 21:46:08 +0700

    test: изменить тесты в соответствие с новой логикой

--- src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
@@ -1,105 +1,113 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.web.server.ResponseStatusException;
+import ru.yandex.practicum.filmorate.exeption.IncorrectParameterException;
+import ru.yandex.practicum.filmorate.exeption.UserNotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.UserService;
+import ru.yandex.practicum.filmorate.storage.InMemoryUserStorage;
+import ru.yandex.practicum.filmorate.storage.UserStorage;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserControllerTest {
+    UserController controller;
+    UserStorage userStorage;
+    UserService userService;
+
+
+    @BeforeEach
+    protected void init() {
+        userStorage = new InMemoryUserStorage();
+        userService = new UserService(userStorage);
+        controller = new UserController(userService);
+    }
 
     @Test
     void getUsers_empty_returnsEmptyList() {
-        UserController c = new UserController();
-        List<User> all = c.getUsers();
+        List<User> all = controller.getUsers();
         assertNotNull(all);
         assertTrue(all.isEmpty());
     }
 
     @Test
     void createUser_valid_setsId() {
-        UserController c = new UserController();
-        User u = user(null, "neo", "Neo");
+        User u =  user(null, "neo", "Neo", "neo@zion.io");
 
-        User saved = c.createUser(u);
+        User saved = controller.createUser(u);
         assertNotNull(saved.getId());
         assertTrue(saved.getId() > 0);
-        assertEquals("neo", c.getUser(saved.getId()).getLogin());
+        assertEquals("neo", controller.getUser(saved.getId()).getLogin());
     }
 
     @Test
     void createUser_blankName_fallbacksToLogin() {
-        UserController c = new UserController();
-        User u = user(null, "trinity", "");
 
-        User saved = c.createUser(u);
+        User u = user(null, "trinity", "","trinity@zion.io");
+
+        User saved = controller.createUser(u);
         assertEquals("trinity", saved.getName());
     }
 
     @Test
     void createUser_loginWithSpaces_throws400() {
-        UserController c = new UserController();
-        User u = user(null, "bad login", "Name");
+        User u = user(null, "bad login", "Name",null);
+        assertThrows(IncorrectParameterException.class, () -> controller.createUser(u));
 
-        ResponseStatusException ex = assertThrows(ResponseStatusException.class, () -> c.createUser(u));
-        assertEquals(400, ex.getStatusCode().value());
-        assertTrue(ex.getReason().contains("login"));
     }
 
     @Test
     void getUser_badId_throws400() {
-        UserController c = new UserController();
-        ResponseStatusException ex = assertThrows(ResponseStatusException.class, () -> c.getUser(-1));
-        assertEquals(400, ex.getStatusCode().value());
+        assertThrows(IncorrectParameterException.class, () -> controller.getUser(-1));
     }
 
     @Test
     void getUser_notFound_throws404() {
-        UserController c = new UserController();
-        ResponseStatusException ex = assertThrows(ResponseStatusException.class, () -> c.getUser(999));
-        assertEquals(404, ex.getStatusCode().value());
+        assertThrows(UserNotFoundException.class, () -> controller.getUser(999));
     }
 
     @Test
     void updateUser_badId_throws400() {
-        UserController c = new UserController();
-        User u = user(null, "smith", "Agent");
-        ResponseStatusException ex1 = assertThrows(ResponseStatusException.class, () -> c.updateUser(u));
-        assertEquals(400, ex1.getStatusCode().value());
+
+        User u = user(null, "smith", "Agent", "smith@zion.io");
+        assertThrows(IncorrectParameterException.class, () -> controller.updateUser(u));
 
         u.setId(0L);
-        ResponseStatusException ex2 = assertThrows(ResponseStatusException.class, () -> c.updateUser(u));
-        assertEquals(400, ex2.getStatusCode().value());
+        assertThrows(IncorrectParameterException.class, () -> controller.updateUser(u));
+
     }
 
     @Test
     void updateUser_notFound_throws404() {
-        UserController c = new UserController();
-        User u = user(123L, "smith", "Agent");
-        ResponseStatusException ex = assertThrows(ResponseStatusException.class, () -> c.updateUser(u));
-        assertEquals(404, ex.getStatusCode().value());
+
+        User u = user(123L, "smith", "Agent", "smith@zion.io");
+        assertThrows(UserNotFoundException.class, () -> controller.updateUser(u));
+
     }
 
     @Test
     void updateUser_valid_updates() {
-        UserController c = new UserController();
-        User u = user(null, "morpheus", "Morpheus");
-        User created = c.createUser(u);
+
+        User u = user(null, "morpheus", "Morpheus","morpheus@zion.io");
+        User created = controller.createUser(u);
 
         created.setName("Captain");
-        User updated = c.updateUser(created);
+        User updated = controller.updateUser(created);
 
         assertEquals(created.getId(), updated.getId());
         assertEquals("Captain", updated.getName());
     }
 
-    private static User user(Long id, String login, String name) {
+    private static User user(Long id, String login, String name, String email) {
         User u = new User();
         u.setId(id);
         u.setLogin(login);
         u.setName(name);
+        u.setEmail(email);
         return u;
     }
 }
