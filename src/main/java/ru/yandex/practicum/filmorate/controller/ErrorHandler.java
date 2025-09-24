633afb2008570603669c87f2b8a5ddb18157eedd
Kolidgio <>
2025-09-24 21:47:28 +0700

    feat: добавить ErrorHandler и ErrorResponse

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -1,2 +1,35 @@
-package ru.yandex.practicum.filmorate.controller;public class ErrorHandelr {
+package ru.yandex.practicum.filmorate.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.yandex.practicum.filmorate.exeption.FilmNotFoundException;
+import ru.yandex.practicum.filmorate.exeption.IncorrectParameterException;
+import ru.yandex.practicum.filmorate.exeption.UserAlreadyExistsException;
+import ru.yandex.practicum.filmorate.exeption.UserNotFoundException;
+
+@RestControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler(FilmNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleNotFound(FilmNotFoundException e) {
+        return new ErrorResponse("Фильм не найден", e.getMessage());
+    }
+    @ExceptionHandler(UserNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleNotFound(UserNotFoundException e) {
+        return new ErrorResponse("Пользователь не найден", e.getMessage());
+    }
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ErrorResponse handleConflict(UserAlreadyExistsException e) {
+        return new ErrorResponse("Пользователь уже создан", e.getMessage());
+    }
+    @ExceptionHandler(IncorrectParameterException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleIncorrect(IncorrectParameterException e) {
+        return new ErrorResponse(String.format("Некорректный параметр %s", e.getParameter()), e.getMessage());
+    }
 }
